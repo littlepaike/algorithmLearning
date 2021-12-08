d29ab32a71a9fa3c01df765303f79fadfd0c1686
littlepaike <f335125303@163.com>
2021-12-08 21:07:23 +0800

    mission accomplished about FCM

--- FCM/myfcm.py
+++ FCM/myfcm.py
@@ -1,10 +1,8 @@
 from pylab import *
 from numpy import *
-import pandas as pd
 import numpy as np
 import operator
 import math
-import matplotlib.pyplot as plt
 import random
 from sklearn.datasets import load_iris
 import pandas as pd
--- FCM/mykmeans.py
+++ FCM/mykmeans.py
@@ -1,6 +1,4 @@
 import copy
-import this
-
 import numpy as np
 import math
 import random
